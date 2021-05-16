7b1588fcacc8fb981f2721ab3d7ea27792f08c30
Baptiste CHOCOT <baptiste.chocot@gmail.com>
2021-05-16 15:00:19 +0100

    fix deprecated piece of code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def active?(a)
     begin
-      current_page?(controller: 'pages', action: a) ? ' active' : ''
+      current_page?({controller: 'pages', action: a}) ? ' active' : ''
     rescue
-      current_page?(controller: 'twitch', action: a) ? ' active' : ''
+      current_page?({controller: 'twitch', action: a}) ? ' active' : ''
     end
   end
 end
