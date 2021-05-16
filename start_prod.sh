856a17ef3923dc4a8232efe10ed822e9b3288c39
Baptiste CHOCOT <baptiste.chocot@gmail.com>
2021-05-16 15:08:34 +0100

    update prod start script

--- start_prod.sh
+++ start_prod.sh
@@ -1,1 +1,4 @@
-rails s -e production -b 'ssl://0.0.0.0:3000?key=private/cert/www_chocorp_net.key&cert=private/cert/www_chocorp_net.full.crt'
+CMD="bin/rails s -e production -b 'ssl://0.0.0.0:3000?key=private/cert/www_chocorp_net.key&cert=private/cert/www_chocorp_net.full.crt'"
+SESSION="chocorp"
+
+tmux new-session -s $SESSION "$CMD"
