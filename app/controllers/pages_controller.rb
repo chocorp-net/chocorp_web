494efd58eebe6dfee9dec312fcd8ca308419d6eb
Baptiste CHOCOT <baptiste.chocot@gmail.com>
2021-10-31 20:59:05 +0000

    fix dead link; minor modifs on resume

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def mcmods
-    am = Mod.new(name: "AuthMod", desc: "AuthMod is an authentication Forge mod which allows modded server owners to accept free versions of Minecraft on their server without wondering about security issues. It forces players to login with a password dedicated to the server.", webpage: "https://www.curseforge.com/minecraft/mc-mods/authmod", logo: "https://github.com/Chocorean/authmod/raw/master/src/main/resources/logo.png")
+    am = Mod.new(name: "AuthMod", desc: "AuthMod is an authentication Forge mod which allows modded server owners to accept free versions of Minecraft on their server without wondering about security issues. It forces players to login with a password dedicated to the server.", webpage: "https://www.curseforge.com/minecraft/mc-mods/authmod", logo: "https://raw.githubusercontent.com/Chocorean/authmod/main/src/main/resources/logo.png")
     mc = Mod.new(name: "MoreCommands", desc: "MoreCommands is a Forge mod that adds a few command to your modded Minecraft server", webpage: "https://www.curseforge.com/minecraft/mc-mods/more-commands-mod", logo: "https://github.com/Chocorean/morecommands/raw/master/src/main/resources/assets/morecommands/textures/logo.png")
     ms = Mod.new(name: "MoreSnowballs", desc: "MoreSnowballs adds a few snowballs with unique effects", webpage: "https://www.curseforge.com/minecraft/mc-mods/more-snowballs", logo: "https://media-cdn.overwolf.wtf/avatars/176/960/636767575153316345.png")
     @mods = [am, mc, ms]
@@ -35,6 +35,7 @@ class PagesController < ApplicationController
       if not Rails.env.production?
         raise err
       end
+      p err
       render status: 500
     end
   end
