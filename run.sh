e5e5e335e35c8a0d535292574991d17219ccc77d
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2022-02-12 18:33:26 -0500

    rework start script; requires tmux

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ -n $1 ]]; then
+  env='development'
+else
+  env='production'
+fi
+
+# create session
+session='chocorp_web'
+tmux new-session -d -s $session || exit 1
+
+# Nginx window
+tmux rename-window -t $session:0 'nginx'
+tmux send-keys -t $session:0 'tail -f /var/log/nginx/access.log' C-m
+tmux split-window -t $session:0 -v
+tmux send-keys -t $session:0 'tail -f /var/log/nginx/error.log' C-m
+
+# Web server, htop and logs
+tmux new-window -t $session:1 -n 'rails'
+tmux send-keys -t $session:1 'RAILS_ENV=$env puma -b unix:///home/pi/chocorp_web/tmp/sockets/puma.sock' C-m
+tmux split-window -t $session:1 -v
+tmux send-keys -t $session:1 "tail -f log/$env.log" C-m
+tmux selectp -t $session:1.0
+tmux splitw -t $session:1 -h
+tmux send-keys -t $session:1 'htop' C-m
+tmux selectp -t $session:1.0
--- start_prod.sh
+++
@@ -1,5 +0,0 @@
-CMD="bin/rails s -e production -b 'ssl://0.0.0.0:3000?key=private/cert/www_chocorp_net.key&cert=private/cert/www_chocorp_net.full.crt'"
-SESSION="chocorp"
-
-./bin/rake assets:precompile
-tmux new-session -s $SESSION "$CMD"
