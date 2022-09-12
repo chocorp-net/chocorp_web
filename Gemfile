0e216780142df4284cc5a2165fe236719e29ef20
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2022-09-11 23:50:35 -0400

    End migration to rails 7; fix #6; close contact for rework

--- Gemfile
+++ Gemfile
@@ -1,26 +1,23 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-
 ruby '3.1.1'
 
 # Minimum stuff
 gem "rails", "~> 7.0"
 gem "sprockets-rails", "~> 3.4"
-gem "sqlite3", "~> 1.5"
+gem "sqlite3", "~> 1.5", git: "https://github.com/sparklemotion/sqlite3-ruby"
 gem "puma", "~> 5.6"
 gem "importmap-rails", "~> 1.1"
 gem "turbo-rails", "~> 1.1"
 gem "stimulus-rails", "~> 1.1"
 gem "jbuilder", "~> 2.11"
 gem "bootsnap", "~> 1.13", require: false
-
-group :development, :test do
-    #gem "debug", platforms: %i[ mri mingw x64_mingw ]
-end
+gem "sass-rails", "~> 6.0"
 
 group :development do
     gem 'web-console'
+    gem 'foreman'
 end
 
 group :test do
@@ -32,4 +29,3 @@ group :test do
     gem "rubocop-rails"
 end
 
-gem "sass-rails", "~> 6.0"
--- app/controllers/twitch_controller.rb
+++ app/controllers/twitch_controller.rb
@@ -7,8 +7,75 @@ class TwitchController < ApplicationController
 
   # GET /brrr
   def brrr
+    query
     @is_printing = printing?
     @is_alive = alive?
     @job_name = job_name
+  end
+
+  private
+  #################
+  # Custom errors #
+  #################
+  class EnvironmentException < StandardError
+  end
+
+  ########
+  # Init #
+  ########
+  def query
+    @connection = get '/api/connection'
+    @job = get '/api/job'
+  end
+
+  ###################
+  # Boolean getters #
+  ###################
+  def printing?
+    @connection == nil ? false : @connection['current']['state'] == 'Printing'
+  end
+
+  def alive?
+    @connection == nil ? false : @connection['current']['state'] != 'Closed'
+  end
+
+  def job_name
+    def name(n)
+      n == nil ? "<no_job>" : n.split('.')[0..-2].join('.')
+    end
+
+    @job == nil ? "<no_job>" : name(@job['job']['file']['name'])
+  end
+
+  ################
+  # HTTP related #
+  ################
+  def get(route)
+    begin
+      resp = send_request route
+      JSON.parse resp.gsub('=>', ':')
+    rescue => e
+      warn e
+      nil
+    end
+  end
+
+  def send_request(route, _content = '', type = 'GET', headers = {})
+    tries = 5
+    key = ENV.fetch('OCTOPRINT_APIKEY', nil)
+    url = ENV.fetch('OCTOPRINT_URL', nil)
+    if key == nil or url == nil
+      raise EnvironmentException.new "Octoprint envvar not loaded"
+    end
+
+    host = url.include?('://') ? url.split('://')[1] : url
+    headers = { 'Host' => host,
+                'X-Api-Key' => key }.merge(headers)
+
+    # TODO refactor
+    headers_arr = []
+    headers.each { |k,v| headers_arr.append "-H #{[k,v].join ':'}" }
+    res = `curl -s -L #{url}#{route} #{headers_arr.join ' '}`
+    res
   end
 end
